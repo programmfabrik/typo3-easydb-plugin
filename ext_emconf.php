5a2b68546b5016cc282b0dd9e77e5a27f52cb426
Helmut Hummel <info@helhum.io>
2017-06-21 11:10:09 +0200

    Create scaffold

---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+  'title' => 'easydb TYPO3 integration',
+  'description' => 'Integration of easydb asset management in TYPO3 CMS',
+  'category' => 'backend',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'modify_tables' => '',
+  'clearCacheOnLoad' => 0,
+  'author' => 'Helmut Hummel',
+  'author_email' => 'info@helhum.io',
+  'author_company' => 'helhum.io',
+  'version' => '0.1.0',
+  'constraints' => [
+    'depends' => [
+      'typo3' => '7.6.0-8.7.99',
+    ],
+    'conflicts' => [
+    ],
+    'suggests' => [
+    ],
+  ],
+];
