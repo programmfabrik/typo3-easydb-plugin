73452861d88084696046638f9dfc1df06171d3bb
Helmut Hummel <info@helhum.io>
2017-06-23 14:40:23 +0200

    Implement JS for upload button

--- Classes/Hook/FileListButtonHook.php
+++ Classes/Hook/FileListButtonHook.php
@@ -21,12 +21,16 @@ namespace Easydb\Typo3Integration\Hook;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
+use TYPO3\CMS\Backend\Routing\UriBuilder;
 use TYPO3\CMS\Backend\Template\Components\ButtonBar;
 use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Lang\LanguageService;
 
+/**
+ * Adds a button for importing files from easydb to the file list module
+ */
 class FileListButtonHook
 {
     /**
@@ -39,28 +43,105 @@ class FileListButtonHook
      */
     private $languageService;
 
-    public function __construct(IconFactory $iconFactory = null, LanguageService $languageService = null)
+    /**
+     * @var UriBuilder
+     */
+    private $uriBuilder;
+
+    public function __construct(IconFactory $iconFactory = null, LanguageService $languageService = null, UriBuilder $uriBuilder = null)
     {
         $this->iconFactory = $iconFactory ?: GeneralUtility::makeInstance(IconFactory::class);
         $this->languageService = $languageService ?: $GLOBALS['LANG'];
+        $this->uriBuilder = $uriBuilder ?: GeneralUtility::makeInstance(UriBuilder::class);
     }
 
     public function getButtons(array $params, ButtonBar $buttonBar)
     {
         $buttons = $params['buttons'];
+        // Only add the button to file list module
+        // Strange API that requires to query super globals, but that's how it currently is
+        // At least we have an almost clean way to add additional buttons
         if (!isset($_GET['M']) || 'file_FilelistList' !== $_GET['M']) {
             return $buttons;
         }
         $buttons[ButtonBar::BUTTON_POSITION_LEFT][] = [];
         $buttonBarIndex = count($buttons[ButtonBar::BUTTON_POSITION_LEFT]);
-        $url = 'javascript:alert("Not yet implemented")';
+
         $button = $buttonBar->makeLinkButton();
         $button->setShowLabelText(true);
         $button->setIcon($this->iconFactory->getIcon('actions-document-new', Icon::SIZE_SMALL));
         $button->setTitle($this->languageService->sL('LLL:EXT:easydb/Resources/Private/Language/locallang.xlf:addFiles'));
-        $button->setHref($url);
+        $button->setHref($this->getWindowOpenJs());
+
         $buttons[ButtonBar::BUTTON_POSITION_LEFT][$buttonBarIndex][] = $button;
 
         return $buttons;
+    }
+
+    private function getWindowOpenJs()
+    {
+        // Encoding galore
+        $filePickerArgument = \rawurlencode(\base64_encode(\json_encode(
+            [
+                'callbackurl' => $this->getCallBackUrl(),
+                'extensions' => $this->getAllowedFileExtensions(),
+            ]
+        )));
+        $windowSize = $this->getWindowSize();
+
+        return <<<EOF
+javascript:window.open("http://master.5.easydb.de/?typo3filepicker=$filePickerArgument",
+    "easydb_picker",
+    "width=${windowSize['width']},height=${windowSize['height']},status=0,menubar=0,resizable=1,location=0,directories=0,scrollbars=1,toolbar=0"
+);
+EOF;
+    }
+
+    /**
+     * @return string
+     * @throws \TYPO3\CMS\Backend\Routing\Exception\RouteNotFoundException
+     */
+    private function getCallBackUrl()
+    {
+        return (string)$this->uriBuilder->buildUriFromRoute(
+            'ajax_easydb_import',
+            [
+                'id' => isset($_GET['id']) ? $_GET['id'] : $this->getRootLevelFolder(),
+            ],
+            UriBuilder::ABSOLUTE_URL
+        );
+    }
+
+    /**
+     * TODO: take allowed file extensions from user permissions
+     *
+     * @return array
+     */
+    private function getAllowedFileExtensions()
+    {
+        return ['jpg', 'tif', 'png'];
+    }
+
+    /**
+     * TODO: make window size configurable
+     *
+     * @return array
+     */
+    private function getWindowSize()
+    {
+        return [
+            'width' => 650,
+            'height' => 600,
+        ];
+    }
+
+    /**
+     * TODO: FIXME We need to look up the root level folder like file list module does
+     *
+     * @return string
+     */
+    private function getRootLevelFolder()
+    {
+        return '1:/';
     }
 }
