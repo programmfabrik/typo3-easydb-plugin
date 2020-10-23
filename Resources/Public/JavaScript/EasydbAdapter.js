7a94b16f0a0957a4d2b2f68c9887dd543dffe17b
Helmut Hummel <info@helhum.io>
2020-10-23 12:52:09 +0200

    Format JS with sapces

--- Resources/Public/JavaScript/EasydbAdapter.js
+++ Resources/Public/JavaScript/EasydbAdapter.js
@@ -1,54 +1,54 @@
 /**
  * Module: TYPO3/CMS/Easydb/EasydbAdapter
  */
-define(['jquery'], function($) {
-	'use strict';
+define(['jquery'], function ($) {
+    'use strict';
 
-	var easydbAdapter = {
+    var easydbAdapter = {
 
-		openPicker: function(event) {
-			event.preventDefault();
-			var $arguments = $(event.target).data('arguments');
-			easydbAdapter.filePickerWindow = window.open(
-				$arguments['targetUrl'],
-				'easydb_picker',
-				'width=' + $arguments['window']['width'] + ',height=' + $arguments['window']['height'] + ',status=0,menubar=0,resizable=1,location=0,directories=0,scrollbars=1,toolbar=0'
-			);
-		},
+        openPicker: function (event) {
+            event.preventDefault();
+            var $arguments = $(event.target).data('arguments');
+            easydbAdapter.filePickerWindow = window.open(
+                    $arguments['targetUrl'],
+                    'easydb_picker',
+                    'width=' + $arguments['window']['width'] + ',height=' + $arguments['window']['height'] + ',status=0,menubar=0,resizable=1,location=0,directories=0,scrollbars=1,toolbar=0'
+            );
+        },
 
-		/**
-		 * Close the easydb file picker if we have a reference to it
-		 */
-		closePicker: function() {
-			if (easydbAdapter.filePickerWindow) {
-				easydbAdapter.filePickerWindow.close();
-				easydbAdapter.filePickerWindow = null;
-			}
-		},
+        /**
+         * Close the easydb file picker if we have a reference to it
+         */
+        closePicker: function () {
+            if (easydbAdapter.filePickerWindow) {
+                easydbAdapter.filePickerWindow.close();
+                easydbAdapter.filePickerWindow = null;
+            }
+        },
 
-		handleMessageEvent: function(event) {
-			if (event.data['easydb']) {
-				if (event.data['easydb']['action'] === 'reload') {
-					window.location.reload();
-				}
-				if (event.data['easydb']['action'] === 'close') {
-					easydbAdapter.closePicker();
-				}
-			}
-		},
+        handleMessageEvent: function (event) {
+            if (event.data['easydb']) {
+                if (event.data['easydb']['action'] === 'reload') {
+                    window.location.reload();
+                }
+                if (event.data['easydb']['action'] === 'close') {
+                    easydbAdapter.closePicker();
+                }
+            }
+        },
 
-		/**
-		 * Add event listeners
-		 */
-		addEventListeners: function () {
-			window.addEventListener('message', this.handleMessageEvent);
-			$(function() {
-				$('.button__file-list-easydb').on('click', easydbAdapter.openPicker);
-			});
-		}
-	};
+        /**
+         * Add event listeners
+         */
+        addEventListeners: function () {
+            window.addEventListener('message', this.handleMessageEvent);
+            $(function () {
+                $('.button__file-list-easydb').on('click', easydbAdapter.openPicker);
+            });
+        }
+    };
 
-	easydbAdapter.addEventListeners();
+    easydbAdapter.addEventListeners();
 
-	return easydbAdapter;
+    return easydbAdapter;
 });
