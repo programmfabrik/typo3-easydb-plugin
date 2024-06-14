9fd3e9388124007054068931944b6f332b5290c6
Helmut Hummel <typo3@helhum.io>
2024-06-14 13:35:51 +0200

    [BUGFIX] Use correct versions in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,25 +1,23 @@
 <?php
-$EM_CONF[$_EXTKEY] = [
-  'title' => 'easydb TYPO3 integration',
-  'description' => 'Integration of easydb asset management in TYPO3 CMS',
-  'category' => 'backend',
-  'state' => 'stable',
-  'uploadfolder' => 0,
-  'createDirs' => '',
-  'modify_tables' => '',
-  'clearCacheOnLoad' => 0,
-  'author' => 'Helmut Hummel',
-  'author_email' => 'info@helhum.io',
-  'author_company' => 'helhum.io',
-  'version' => '2.0.0',
-  'constraints' => [
-    'depends' => [
-      'typo3' => '11.5.0-11.5.99',
-      'php' => '7.4.0-8.99.99',
+$EM_CONF['easydb'] = [
+    'title' => 'easydb TYPO3 integration',
+    'description' => 'Integration of easydb asset management in TYPO3 CMS',
+    'category' => 'backend',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Helmut Hummel',
+    'author_email' => 'info@helhum.io',
+    'author_company' => 'helhum.io',
+    'version' => '3.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.36-12.4.99',
+            'php' => '7.4.0-8.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
     ],
-    'conflicts' => [
-    ],
-    'suggests' => [
-    ],
-  ],
 ];
